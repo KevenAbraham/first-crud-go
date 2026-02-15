dca1b28be04b24cb5e955de03cadeeec7a5aa31d
KevenAbraham <abraham.keven20@gmail.com>
2026-02-15 18:21:31 -0300

    Update create handler

--- src/handler/createUser.go
+++ src/handler/createUser.go
@@ -1,7 +1,21 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/KevenAbraham/first-crud-go/src/configurations/error_mapping"
+	"github.com/KevenAbraham/first-crud-go/src/handler/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(ctx *gin.Context) {
-	
+	var userRequest request.UserRequest
+	if err := ctx.ShouldBindJSON(&userRequest); err != nil {
+		restErr := error_mapping.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error: %s\n", err.Error()))
+		
+		ctx.JSON(restErr.Code, restErr)
+	}
+
+	fmt.Println(userRequest)
 }
